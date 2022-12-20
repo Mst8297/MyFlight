d21912b7dfe8dfd07f35ea7fbb3a62b78110cb03
ester <esterm8297@gmail.com>
2022-12-20 13:28:08 +0200

    admin user initialization

--- MyFlight/Controllers/AdminUserController.cs
+++ MyFlight/Controllers/AdminUserController.cs
@@ -6,8 +6,9 @@ using System.Web.Mvc;
 using MyFlight.Models;
 using System.Data.Entity;
 using MyFlight.DAL;
-
-
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace MyFlight.Controllers
 
@@ -15,10 +16,48 @@ namespace MyFlight.Controllers
     public class AdminUserController : Controller
     {
         // GET: AdminUser
-        
+        private AdminUserDAL db = new AdminUserDAL();
+
         public ActionResult FlightListEdit()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            AdminUser au = new AdminUser();
+            return View("Submit", au);
+            
+              
+        }
+
+        public ActionResult Submit(AdminUser au)
+        {
+            if (ModelState.IsValid)
+            {
+                if (au.UserName == "Admin" && au.Password == "Admin123")
+                {
+                    return View("FlightListEdit");
+                }
+                else
+                {
+                    MessageBox.Show("The user name or password is not correct, please try again!", "Wrong values", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return View();
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("The user name or password is not correct, please try again!", "Wrong values", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return View();
+            }
+
+        }
+        public ActionResult GetPopUp(int id)
+        {
+            // Communication with the model
+            return View("UserControl/PopupView"); // The defined View in the project
         }
     }
 }
