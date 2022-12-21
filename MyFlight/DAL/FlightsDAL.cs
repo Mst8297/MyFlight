5e1ec2919ac4b52c540d6ae98716361a8be09d04
ester <esterm8297@gmail.com>
2022-12-21 19:04:05 +0200

    flightsDAL initialization

---
+++ MyFlight/DAL/FlightsDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyFlight.Models;
+using System.Data.Entity;
+
+
+
+namespace MyFlight.DAL
+{
+    public class FlightsDAL :DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Flights>().ToTable("Flightstbl");
+        }
+        public DbSet<Flights> flight { get; set; }
+    }
+}
---
+++ MyFlight/Models/Flights.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFlight.Models
+{
+    public class Flights
+    {
+        public int FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime FlightDate { get; set; }
+        public DateTime Hour { get; set; }
+        public int NumOfEvaliableSeats { get; set; }
+        public int TotalSeatsAmount { get; set; }
+
+
+
+
+    }
+}
